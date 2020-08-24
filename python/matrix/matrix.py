03baccad45617c2a49fb0755f36248efe65d924a
TrimChap <trimchap@trishtunney.com>
2020-08-23 20:29:45 -0700

    matrix (incomplete)

---
+++ python/matrix/matrix.py
@@ -0,0 +1,9 @@
+class Matrix:
+    def __init__(self, matrix_string):
+        self.matrix_string = matrix_string
+
+    def row(self, index):
+        pass
+
+    def column(self, index):
+        pass
